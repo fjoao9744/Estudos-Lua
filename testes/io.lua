c5bb955274c0e8d715907a9bbacc612bd2d27a2d
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-03-17 16:37:41 -0300

    5

---
+++ testes/io.lua
@@ -0,0 +1,10 @@
+
+local num = io.read() -- Cria uma entrada
+--[[ argumentos do .read()
+"*n" -> numero
+"*l" -> texto
+"*a" -> o resto da entrada
+"*line" -> lê uma linha, incluindo espaços, mas sem incluir a quebra de linha final
+]]--
+
+print(num)
