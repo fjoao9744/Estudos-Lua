074dfb26e2bd2345a12488626d7510fed1a727fc
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-03-19 12:44:14 -0300

    Create for2.lua

---
+++ testes/for2.lua
@@ -0,0 +1,13 @@
+--[[
+generico:
+for chave, valor in iterador(pairs e ipairs) do
+    -- codigo
+end
+
+]]--
+
+t = {nome="joao", idade=18}
+
+for chave, valor in t do
+    print(chave, valor)
+end
