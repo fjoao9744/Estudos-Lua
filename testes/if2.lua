9131804db8f92c677ecdab5a318b887026f64896
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-03-19 10:15:21 -0300

    6

---
+++ testes/if2.lua
@@ -0,0 +1,15 @@
+nota = io.read("*n")
+
+if nota >= 7 then
+    print("Sua nota esta boa")
+
+elseif nota >= 5 then
+    print("Tem que melhorar!")
+
+elseif nota >= 3 then
+    print("Sua nota esta ruim!")
+
+else
+    print("Você esta horrivel!!!")
+
+end
