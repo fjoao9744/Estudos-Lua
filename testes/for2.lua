469b40870f64927351bc868f6740efd4670531fe
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-03-19 12:45:49 -0300

    Create for2.lua

--- testes/for2.lua
+++ testes/for2.lua
@@ -6,8 +6,14 @@ end
 
 ]]--
 
-t = {nome="joao", idade=18}
+t1 = {nome="joao", idade=18}
+t2 = {13, 40, 20, 1, -3, 50}
 
-for chave, valor in t do
+
+for chave, valor in pairs(t1) do
     print(chave, valor)
+end
+
+for indice, valor in ipairs(t2) do
+    print(indice, valor)
 end
