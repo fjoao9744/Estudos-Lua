07005cc08ca4054b3880bc42994d218abd2f0c5a
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-03-19 10:37:46 -0300

    7

---
+++ testes/if3.lua
@@ -0,0 +1,11 @@
+function is_empty(table)
+    if #table == 0 then print("A lista esta vazia") else print("A lista possui itens") end
+    
+end
+
+t1 = {2, 4, 5}
+t2 = {}
+
+is_empty(t1)
+is_empty(t2)
+    
