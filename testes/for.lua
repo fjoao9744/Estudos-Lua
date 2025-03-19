ce5bc427b720773db7c1f603cd3dc1f4973ba1b1
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-03-19 10:55:56 -0300

    Create for.lua

---
+++ testes/for.lua
@@ -0,0 +1,21 @@
+-- no lua eistem 2 tipos de for(numerico e generico)
+
+--[[
+numerico:
+for i = inicio, fim, incremento do
+    -- codigo
+end
+
+]]--
+
+for i = 1, 10 do 
+    print(i)
+end
+
+for i = 0, 10, 2 do
+    print(i)
+end
+
+for i = 10, 0, -1 do
+    print(i)
+end
