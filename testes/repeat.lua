b9ae190e0f9be50fc603f341c957ad9c45f1a77e
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-03-21 11:21:46 -0300

    10

---
+++ testes/repeat.lua
@@ -0,0 +1,16 @@
+i = 0
+
+repeat -- vai repetir no minimo uma vez
+    print(i)
+    i = i + 1
+    
+until i >= 10 -- repete até a condição se tornar verdadeira 
+
+i = 0
+
+while i <= 10 do -- repete se a condição for verdadeira
+    print(i)
+    i = i + 1
+
+end
+    
