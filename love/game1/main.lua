c3d8406a1b1d9d97b66452e2a5cc1d0e81566a0a
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-04-17 11:34:12 -0300

    Add files via upload

---
+++ love/game1/main.lua
@@ -0,0 +1,18 @@
+-- personagem
+player = {
+    x = 100,
+    y = 100,
+    speed = 200
+}
+
+function love.load()
+
+end
+
+function love.update(dt) -- "delta time" o intervalo de tempo de um frame para outro
+    
+end
+
+function love.draw()
+    love.graphics.rectangle("fill", 100, 100, 100, 100)
+end
